996cc5cad99830f1a1c96280f2890159c6ce2732
Tay West <tayanne.west@gmail.com>
2021-10-02 16:25:50 -0400

    accept correct user input

--- app.js
+++ app.js
@@ -4,19 +4,53 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
-  getGuess() {
-    let currentGuess = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-    while (getGuess) {
-      // parseInt(this.getGuess())
-    }
-    console.log()
-  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+  },
+  getGuess() {
+    console.log(this.secretNum)
+    let currentGuess = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+    // const parsed = parseInt(this.currentGuess)
+      while (currentGuess > this.biggestNum || currentGuess < this.smallestNum) {
+        currentGuess = prompt (`gotta be between ${this.smallestNum} and ${this.biggestNum} pal`)
+        }
+  },
 }
 
-game.getGuess()
+// game.getGuess()
+// game.play()
+// console.log(game.secretNum)
 
-// while currentGuess !== secretNum, getGuess
+
+// const parsed = parseInt(this.currentGuess)
+// while (parsed !== this.secretNum) {
+//   return this.getGuess()
+// }
+
+game.play()
+
+// const game = {
+//   title: 'Guess the Number!',
+//   biggestNum: 100,
+//   smallestNum: 1,
+//   secretNum: null,
+//   prevGuesses: [],
+//   getGuess() {
+//     let userInput = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`); {
+//       while (userInput > this.biggestNum || userInput < this.smallestNum) {
+//         userInput = prompt (`That isn't how you play this game cowboy, the guess must be between ${this.biggestNum} and ${this.smallestNum}. Please guess again.`)
+//       }
+//       // return userInput
+//     }  // function sortGuess(userInput) 
+// },
+//   play: function() {
+//     this.secretNum = Math.floor(Math.random() * 
+//       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+//       while (smallestNum >= 1 && biggestNum <= 100) {
+//         ;
+//       }
+//     },
+// }
+
+// game.getGuess()
