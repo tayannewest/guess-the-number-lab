93f12f0b8f36d7b8dfecd85cd61709db83b603d9
Tay West <tayanne.west@gmail.com>
2021-10-03 15:46:00 -0400

    complete step 4

--- app.js
+++ app.js
@@ -1,56 +1,29 @@
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  biggestNum: 5,
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  },
+      do {
+        this.prevGuesses.push(this.getGuess()) 
+      } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
+      console.log(this.prevGuesses)
+    },
   getGuess() {
-    console.log(this.secretNum)
-    let currentGuess = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-    // const parsed = parseInt(this.currentGuess)
-      while (currentGuess > this.biggestNum || currentGuess < this.smallestNum) {
-        currentGuess = prompt (`gotta be between ${this.smallestNum} and ${this.biggestNum} pal`)
-        }
-  },
-}
-
-// game.getGuess()
-// game.play()
-// console.log(game.secretNum)
-
-
-// const parsed = parseInt(this.currentGuess)
-// while (parsed !== this.secretNum) {
-//   return this.getGuess()
-// }
-
-game.play()
-
-// const game = {
-//   title: 'Guess the Number!',
-//   biggestNum: 100,
-//   smallestNum: 1,
-//   secretNum: null,
-//   prevGuesses: [],
-//   getGuess() {
-//     let userInput = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`); {
-//       while (userInput > this.biggestNum || userInput < this.smallestNum) {
-//         userInput = prompt (`That isn't how you play this game cowboy, the guess must be between ${this.biggestNum} and ${this.smallestNum}. Please guess again.`)
-//       }
-//       // return userInput
-//     }  // function sortGuess(userInput) 
-// },
-//   play: function() {
-//     this.secretNum = Math.floor(Math.random() * 
-//       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-//       while (smallestNum >= 1 && biggestNum <= 100) {
-//         ;
-//       }
-//     },
-// }
-
-// game.getGuess()
+    let currentGuess = null
+    while (currentGuess > this.biggestNum || currentGuess < this.smallestNum || isNaN(currentGuess)) {
+      currentGuess = parseInt( 
+        prompt (`Guess a number between ${this.smallestNum} and ${this.biggestNum} to play!`))
+      }
+      return currentGuess
+    },
+  }
+  
+  
+  
+    // game.getGuess()
+    game.play()
+    
